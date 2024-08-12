dfec7fc8c35db864f098ac8ee2a687d93b5e6ae6
Your Name <ravipandey@elitemindz.co>
2024-08-12 19:00:46 +0530

    code for enquiry form

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ContactService } from '../../core/services/my-shared.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -12,7 +13,11 @@ export class HomeComponent {
   contactForm: FormGroup;
   submitted = false;
 
-  constructor(private fb: FormBuilder, private contactService: ContactService) {
+  constructor(
+    private fb: FormBuilder,
+    private contactService: ContactService,
+    private toastr: ToastrService  // Inject ToastrService
+  ) {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -29,16 +34,19 @@ export class HomeComponent {
       this.contactService.postContactForm(this.contactForm.value).subscribe(
         response => {
           console.log('Form submitted successfully', response);
+          this.toastr.success('Your message has been sent successfully!', 'Success');
           // Handle success response
         },
         error => {
           console.error('Error submitting form', error);
+          this.toastr.error('There was an error sending your message. Please try again.', 'Error');
           // Handle error response
         }
       );
 
     } else {
       console.log('Form is invalid');
+      this.toastr.warning('Please fill out all required fields.', 'Form Incomplete');
     }
   }
 }
