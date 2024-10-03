3c7d677c8bab8994fb6a9bfe9c9f5c4364eb951c
raviEM <ravipandey@elitemindz.co>
2024-10-03 18:47:03 +0530

    code for burger menue

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,48 +1,55 @@
-import {Component, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
-
+import { Component, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'] // Make sure to use 'styleUrls'
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
   mobileNavActive: boolean = false;
+
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
-ngOnInit(): void {
-  this.addClickListeners();
 
-}
-
-@HostListener('window:scroll', ['$event'])
-onScroll(event: Event): void {
-  if (window.scrollY > 100) {
-    this.renderer.addClass(this.elementRef.nativeElement.querySelector('.header'), 'scrolled');
-    this.renderer.addClass(this.elementRef.nativeElement.querySelector('.scroll-top'), 'active');
-  } else {
-    this.renderer.removeClass(this.elementRef.nativeElement.querySelector('.header'), 'scrolled');
-    this.renderer.removeClass(this.elementRef.nativeElement.querySelector('.scroll-top'), 'active');
+  ngOnInit(): void {
+    this.addClickListeners();
   }
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    if (window.scrollY > 100) {
+      this.renderer.addClass(this.elementRef.nativeElement.querySelector('.header'), 'scrolled');
+      this.renderer.addClass(this.elementRef.nativeElement.querySelector('.scroll-top'), 'active');
+    } else {
+      this.renderer.removeClass(this.elementRef.nativeElement.querySelector('.header'), 'scrolled');
+      this.renderer.removeClass(this.elementRef.nativeElement.querySelector('.scroll-top'), 'active');
+    }
+  }
 
-}
-
-private addClickListeners(): void {
-  const faqItems = this.elementRef.nativeElement.querySelectorAll('.faq-item h3, .faq-item .faq-toggle');
-  faqItems.forEach((faqItem: { parentNode: { classList: { toggle: (arg0: string) => void; }; }; }) => {
-    this.renderer.listen(faqItem, 'click', () => {
-      faqItem.parentNode.classList.toggle('faq-active');
+  private addClickListeners(): void {
+    const faqItems = this.elementRef.nativeElement.querySelectorAll('.faq-item h3, .faq-item .faq-toggle');
+    faqItems.forEach((faqItem: { parentNode: { classList: { toggle: (arg0: string) => void; }; }; }) => {
+      this.renderer.listen(faqItem, 'click', () => {
+        faqItem.parentNode.classList.toggle('faq-active');
+      });
     });
-  });
-}
+  }
 
+  toggleMobileNav() {
+    this.mobileNavActive = !this.mobileNavActive;
 
-toggleMobileNav() {
-  this.mobileNavActive = !this.mobileNavActive;
-}
+    if (this.mobileNavActive) {
+      this.renderer.addClass(document.body, 'mobile-nav-active');  // Add class to body to avoid conflicts with scroll
+    } else {
+      this.renderer.removeClass(document.body, 'mobile-nav-active');
+    }
+  }
 
-scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
+  closeMobileNav() {
+    this.mobileNavActive = false;
+    this.renderer.removeClass(document.body, 'mobile-nav-active');
+  }
 
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
