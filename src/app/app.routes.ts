1d99f7aa9660d6d479bd351b052176627bd05781
raviEM <ravipandey@elitemindz.co>
2024-11-09 15:03:16 +0530

    code for dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { AccountingServicesComponent } from './home/accounting-services/accounti
 import { TaxByRegionComponent } from './home/tax-by-region/tax-by-region.component';
 import { TermsAndConditionsComponent } from './home/terms-and-conditions/terms-and-conditions.component';
 import { PrivacyPolicyComponent } from './home/privacy-policy/privacy-policy.component';
+import { DashboardComponent } from './home/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -30,6 +31,8 @@ export const routes: Routes = [
   {path: 'tax-by-region', component: TaxByRegionComponent},
   {path: 'terms-of-service', component: TermsAndConditionsComponent},
   {path: 'privacy-policy', component: PrivacyPolicyComponent},
+  {path: 'dashboard', component: DashboardComponent},
+
 ];
 
 @NgModule({
